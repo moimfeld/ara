17ecc945f0b6e71772be629158d97d6e594450d2
Moritz Imfeld <moimfeld@student.ethz.ch>
2023-08-01 22:18:19 +0200

    Add header guarding
    Signed-off-by: Moritz Imfeld <moimfeld@student.ethz.ch>

--- apps/custom_model/kernel/custom_model_helper.h
+++ apps/custom_model/kernel/custom_model_helper.h
@@ -1,3 +1,6 @@
+#ifndef _CUSTOM_MODEL_HELPER_H_
+#define _CUSTOM_MODEL_HELPER_H_
+
 #include <stdio.h>
 void getFloatUpTo5thDecimal(float num, char resultStr[8]) {
     // Convert float to integer by multiplying by 100000
@@ -11,3 +14,5 @@ void getFloatUpTo5thDecimal(float num, char resultStr[8]) {
     // Create the formatted string (up to 4th decimal place)
     sprintf(resultStr, "%d.%05d", integerPart, decimalPart);
 }
+
+#endif
