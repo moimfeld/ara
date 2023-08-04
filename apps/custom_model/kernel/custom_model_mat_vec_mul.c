33346e78a4a91d66c24d0cc3b887044330cb0cda
Moritz Imfeld <moimfeld@student.ethz.ch>
2023-08-04 12:21:04 +0200

    Revise mat-vec kernel
    Signed-off-by: Moritz Imfeld <moimfeld@student.ethz.ch>

--- apps/custom_model/kernel/custom_model_mat_vec_mul.c
+++ apps/custom_model/kernel/custom_model_mat_vec_mul.c
@@ -55,6 +55,11 @@ void slow_mat_vec_mul(const uint32_t n_rows,
     }
 }
 
+// Written using inline assembly
+// vector regirster usage in function:
+// v0: result vector (m8)
+// v8: input (m8)
+// v16: weights, multiplication results (m8)
 void mat_vec_mul_n_columns_smaller_vl_max(const uint32_t n_rows,
                                           const uint32_t n_columns,
                                           const float   weights[n_rows][n_columns],
@@ -64,42 +69,44 @@ void mat_vec_mul_n_columns_smaller_vl_max(const uint32_t n_rows,
 {
     // Initialize
     float * w_ptr = (float * ) weights;
-    size_t vl = vsetvl_e32m8(n_columns);
-    vfloat32m8_t acc_vec;
-    vfloat32m8_t inp_vec;
-    vfloat32m1_t res_vec;
+    size_t vl;
+    asm volatile("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(vl) :"r"(n_columns));
 
     // loading the input vector only once
-    inp_vec = vle32_v_f32m8(input, vl);
+    asm volatile("vle32.v v8, (%0)" ::"r"(input)); // load input once
+    // inp_vec = vle32_v_f32m8(input, vl);
 
     for (uint32_t i = 0; i < n_rows; i++) {
+        // FOR TESTING
+        asm volatile("vsetvli %0, %1, e32, m8, ta, ma" : "=r"(vl) :"r"(n_columns));
+
         DEBUG_PRINTF("mat_vec_mul_n_columns_smaller_vl_max: Starting to process %0dth row\n", i)
-        // Initialization
-        vsetvl_e32m8(n_columns);
-        acc_vec = vfsub_vv_f32m8(acc_vec, acc_vec, vl);
-        res_vec = vfsub_vv_f32m1(res_vec, res_vec, 1);
+         // load weights
+        asm volatile("vle32.v v16, (%0)" :: "r"(w_ptr));
+
+         // init result vector to 0
+        asm volatile("vmv.s.x v0, x0");
 
         // computation
-        acc_vec = vfmacc_vv_f32m8(acc_vec, vle32_v_f32m8(w_ptr, vl), inp_vec, vl);
+        asm volatile("vfmul.vv v16, v16, v8");
 
         // Updating pointers
         w_ptr += vl;
 
-        // Accumulate accumulators (reduction)
-        res_vec = vfredosum_vs_f32m8_f32m1(res_vec, acc_vec, res_vec, n_columns);
+        // Sum up multiplications
+        asm volatile("vfredusum.vs v0, v16, v0");
+
+        // store result in output buffer
+        // asm volatile("vfmv.f.s %0, v0" : "=f"(output[i])); // CANNOT USE MOVE.....
         
         // Adjust vl for bias addition
-        size_t vl = vsetvl_e32m1(1);
-
-        // Add bias
-        res_vec = vfadd_vf_f32m1(res_vec, bias[i], 1);
+        asm volatile("vsetivli zero, 1, e32, m1, ta, ma");
 
         // Store result
-        vse32_v_f32m1(&output[i], res_vec, 1);
+        asm volatile("vse32.v v0, (%0)"::"r"(&output[i]));
 
-        // WHY DOES THIS LINE NOT REPLACE THE THREE VECTOR INSTRUCTIONS ABOVE?????? (IN SPIKE IT WORKS)
-        // output_[i] = vfmv_f_s_f32m1_f32(res_vec) + bias[i];
-        // printf("output[%0d] = %f", i, a);
+        // Add bias
+        output[i] = res + bias[i];
     }
     return;
 }
