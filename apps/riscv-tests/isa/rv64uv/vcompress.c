adfc64b2d36e84d4d8a623d752e95b82f41b89f4
Moritz Imfeld <moimfeld@student.ethz.ch>
2024-04-05 14:27:43 +0200

    [apps] Add vrgather and vcompress to riscv_tests.
    Signed-off-by: Moritz Imfeld <moimfeld@student.ethz.ch>

--- apps/riscv-tests/isa/rv64uv/vcompress.c
+++ apps/riscv-tests/isa/rv64uv/vcompress.c
@@ -11,16 +11,29 @@ void TEST_CASE1() {
   VSET(4, e64, m1);
   VLOAD_64(v4, 1, 2, 3, 4);
   VLOAD_64(v0, 12, 0, 0, 0);
-  CLEAR(v2);
+  VCLEAR(v2);
   __asm__ volatile("vcompress.vm v2, v4, v0");
-  DEBUG_64(v2);
-  VEC_CMP_64(1, v2, 3, 4, 0, 0);
+  VCMP_U64(1, v2, 3, 4, 0, 0);
 }
+
+void TEST_CASE2() {
+  VSET(16, e8, m1);
+  VLOAD_8(v4, 1, 2, 3, 4,
+              5, 6, 7, 8,
+              9, 10, 11, 12,
+              13, 14, 15, 16);
+  VLOAD_8(v0, 0b10111100, 0b10111111, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0);
+  VCLEAR(v2);
+  __asm__ volatile("vcompress.vm v2, v4, v0");
+  VCMP_U8(2, v2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 16, 0, 0, 0, 0);
+}
+
 
 int main(void) {
   INIT_CHECK();
   enable_vec();
   enable_fp();
   TEST_CASE1();
+  TEST_CASE2();
   EXIT_CHECK();
 }
